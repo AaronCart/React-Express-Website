b54bef74ebfee7f87384edebff3243ed890c0340
Aaron Cartledge <s3840848@student.rmit.edu.au>
2021-10-12 00:44:53 +1100

    finished database code for users and posts

--- a2-express/src/database/models/user.js
+++ a2-express/src/database/models/user.js
@@ -1,5 +1,3 @@
-const { Sequelize } = require("sequelize/types");
-
 module.exports = (sequelize, DataTypes) =>
   sequelize.define("user", {
     email: {
@@ -14,10 +12,5 @@ module.exports = (sequelize, DataTypes) =>
     fName: {
       type: DataTypes.STRING(40),
       allowNull: false
-    },
-
-    // Get rid of createdAt and updatedAt if the DATEONLY format doesn't work
-    // Get rid of these comments if it does work and instead type, DATEONLY used so that seconds aren't displayed as well
-    createdAt: Sequelize.DATEONLY,
-    updatedAt: Sequelize.DATEONLY
+    }
   });
