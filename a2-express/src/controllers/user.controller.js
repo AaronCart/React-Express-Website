2f055fcce99e0306e08b74630e1fd31bc06918f4
Aaron Cartledge <s3840848@student.rmit.edu.au>
2021-10-12 01:03:15 +1100

    fixed spelling

--- a2-express/src/controllers/user.controller.js
+++ a2-express/src/controllers/user.controller.js
@@ -1,32 +1,32 @@
 const db = require("../database");
 const argon2 = require("argon2");
 
-// Select all users from the database.
+// Select all users from the database
 exports.all = async (req, res) => {
   const users = await db.user.findAll();
 
   res.json(users);
 };
 
-// Select one user from the database.
+// Select one user from the database
 exports.one = async (req, res) => {
   const user = await db.user.findByPk(req.params.id);
 
   res.json(user);
 };
 
-// Select one user from the database if email and password are a match.
+// Select one user from the database if email and password are a match
 exports.signin = async (req, res) => {
   const user = await db.user.findByPk(req.query.email);
 
   if (user === null || await argon2.verify(user.password_hash, req.query.password) === false)
-    // Sign In failed.
+    // Sign In failed
     res.json(null);
   else
     res.json(user);
 };
 
-// Create a user in the database.
+// Create a user in the database
 exports.create = async (req, res) => {
   
   // Hash password from react SignUp.js
