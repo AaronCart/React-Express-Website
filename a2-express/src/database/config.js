407971c40d25f752bef0db4e779039ca290d20a9
Aaron Cartledge <s3840848@student.rmit.edu.au>
2021-10-11 23:43:07 +1100

    added basic database, controllers, and routes folders from week 8 tute

---
+++ a2-express/src/database/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HOST: "rmit.australiaeast.cloudapp.azure.com",
+  USER: "demo_fwp_registration_login_logout",
+  PASSWORD: "abc123",
+  DB: "demo_fwp_registration_login_logout",
+  DIALECT: "mysql"
+};
