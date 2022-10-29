1f145774137a7fdcbc2c4f2e7a9abf51f4831477
AaronCart <115600461+AaronCart@users.noreply.github.com>
2022-10-29 15:56:28 +1100

    Hidden Password

--- a2-express/src/database/config.js
+++ a2-express/src/database/config.js
@@ -1,7 +1,8 @@
+// Old credentials won't work anymore as database and student ID has been deleted so password has been blanked out
 module.exports = {
   HOST: "rmit.australiaeast.cloudapp.azure.com",
   USER: "s3840848_fwp_a2",
-  PASSWORD: "Aar0nc1306%",
+  PASSWORD: "*****",
   DB: "s3840848_fwp_a2",
   DIALECT: "mysql"
 };
